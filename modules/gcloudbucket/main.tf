7f61326b3602eb88852a68e5da0c5b9aabf6389e
Jenkins Automation Server <jenkins@jenkins-server.us-east1-b.c.nimble-willow-223217.internal>
2019-07-18 03:05:00 +0000

    Adding new bucket

--- modules/gcloudbucket/main.tf
+++ modules/gcloudbucket/main.tf
@@ -1,13 +1,13 @@
-data "google_client_config" "default" {}
+data "google_client_config" "jay-test-bucket" {}
 
 provider "google" {
   credentials = "${file("D:/GitHubProjects/jay-terraform/gcp/gcpkeys.json")}"
 }
 
-resource "google_storage_bucket" "default" {
+resource "google_storage_bucket" "jay-test-bucket" {
   count         = "${var.lifecycleenabled == "false" ? 1 : 0}"
   name          = "${var.name}"
-  #location      = "${length(var.region) > 0 ? var.region : data.google_client_config.default.region}"
+  #location      = "${length(var.region) > 0 ? var.region : data.google_client_config.jay-test-bucket.region}"
   project       = "${var.project}"
   storage_class = "${var.storage_class}"
   force_destroy = "${var.force_destroy}"
@@ -19,10 +19,10 @@ resource "google_storage_bucket" "default" {
 
 }
 
-resource "google_storage_bucket" "defaultwithlifecycle" {
+resource "google_storage_bucket" "jay-test-bucketwithlifecycle" {
   count         = "${var.lifecycleenabled == "true" ? 1 : 0}"
   name          = "${var.name}"
-  #location      = "${length(var.region) > 0 ? var.region : data.google_client_config.default.region}"
+  #location      = "${length(var.region) > 0 ? var.region : data.google_client_config.jay-test-bucket.region}"
   project       = "${var.project}"
   storage_class = "${var.storage_class}"
   force_destroy = "${var.force_destroy}"
@@ -42,12 +42,12 @@ resource "google_storage_bucket" "defaultwithlifecycle" {
   }
 }
 
-resource "google_storage_bucket_acl" "default" {
-  #count       = "${var.enabled == "true" ? length(google_storage_bucket.default.*.name) : 0}"
-  default_acl = "${var.default_acl}"
+resource "google_storage_bucket_acl" "jay-test-bucket" {
+  #count       = "${var.enabled == "true" ? length(google_storage_bucket.jay-test-bucket.*.name) : 0}"
+  jay-test-bucket_acl = "${var.default_acl}"
   bucket      = "${coalesce(
-                 element(concat(google_storage_bucket.default.*.name, list("")),0),
-                 element(concat(google_storage_bucket.defaultwithlifecycle.*.name, list("")),0)
+                 element(concat(google_storage_bucket.jay-test-bucket.*.name, list("")),0),
+                 element(concat(google_storage_bucket.jay-test-bucketwithlifecycle.*.name, list("")),0)
                  )}"
   role_entity = [
     "${compact(var.role_entity)}",
